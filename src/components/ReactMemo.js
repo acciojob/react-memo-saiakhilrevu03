dc0e892b7e22b884f409d0c2e00b88c310aa04f6
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-10-04 20:58:28 +0530

    Create ReactMemo.js

---
+++ src/components/ReactMemo.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// Only re-renders when todos prop changes!
+const TodoList = React.memo(function TodoList({ todos }) {
+  return (
+    <ul data-cy="todo-list">
+      {todos.map((todo, idx) => (
+        <li key={idx}>{todo}</li>
+      ))}
+    </ul>
+  );
+});
+
+function ReactMemo({ todos }) {
+  return (
+    <div>
+      <h3>Todos</h3>
+      <TodoList todos={todos} />
+    </div>
+  );
+}
+
+export default ReactMemo;
