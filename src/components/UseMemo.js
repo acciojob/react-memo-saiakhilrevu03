30b70acb0766b70b59d494c66d8a334e521db299
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-10-04 20:56:47 +0530

    Create UseMemo.js

---
+++ src/components/UseMemo.js
@@ -0,0 +1,26 @@
+import React, { useMemo } from "react";
+
+function UseMemo({ counter }) {
+  // Expensive calculation - just to demo useMemo
+  const doubleCounter = useMemo(() => {
+    // Let's pretend this is expensive
+    let result = counter;
+    for (let i = 0; i < 10000000; i++) {
+      result = result;
+    }
+    return counter * 2;
+  }, [counter]);
+
+  return (
+    <div>
+      <p>
+        Counter: <span data-cy="counter">{counter}</span>
+      </p>
+      <p>
+        Double (useMemo): <span data-cy="double-value">{doubleCounter}</span>
+      </p>
+    </div>
+  );
+}
+
+export default UseMemo;
